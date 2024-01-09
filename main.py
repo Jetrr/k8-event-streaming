b53fcf00a0533ce3d8fbb6526736856aa18611e7
Ultra-Lion <rohanakhtar1999@gmail.com>
2024-01-09 17:41:19 +0500

    updated pub/sub topic name for backend integration

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ PROJECT_ID = "jetrr-cloud"
 CLUSTER_LOCATION = "us-central1-f"
 CLUSTER_NAME = "gpu-cluster-auto"
 NAMESPACE = "default"
-TOPIC_NAME = "gke-job-event-stream"
+TOPIC_NAME = "vertex-job-status-update-topic-sub"
 
 credentials = service_account.Credentials.from_service_account_file(
     filename=KEYFILE_PATH,
